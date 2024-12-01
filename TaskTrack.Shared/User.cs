e497c30b71dba2c77d5d395c8eb84a011d76804a
eddyiiz <eddyxsayf@gmail.com>
2024-12-01 11:19:51 +0200

    add model

---
+++ TaskTrack.Shared/User.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskTrack.Shared
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public required byte[] PasswordHash { get; set; }
+        public required byte[] PasswordSalt { get; set; }
+    }
+}
