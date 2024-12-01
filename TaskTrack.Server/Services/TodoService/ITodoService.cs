a4f1aa5172b5b5cea4d5d6e0e37dfb2a7d66e3d3
eddyiiz <eddyxsayf@gmail.com>
2024-12-01 12:52:29 +0200

    add interface

--- TaskTrack.Server/Services/TodoService/ITodoService.cs
+++ TaskTrack.Server/Services/TodoService/ITodoService.cs
@@ -2,7 +2,10 @@
 {
     public interface ITodoService
     {
-
-
+        Task<ServiceResponse<List<Todo>>> FetchTodos(int userId);
+        Task<ServiceResponse<Todo>> FetchTodo(int userId, int todoId);
+        Task<ServiceResponse<Todo>> CreateTodo(int userId, Todo todo);
+        Task<ServiceResponse<Todo>> UpdateTodo(int userId, Todo todo);
+        Task<ServiceResponse<bool>> DeleteTodo(int userId, int todoId);
     }
 }
