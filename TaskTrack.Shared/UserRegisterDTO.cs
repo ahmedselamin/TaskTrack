6219e853c096f3a83e308bd429c8a83893c536cd
eddyiiz <eddyxsayf@gmail.com>
2024-12-01 11:57:50 +0200

    add dto

---
+++ TaskTrack.Shared/UserRegisterDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskTrack.Shared
+{
+    public class UserRegisterDTO
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
