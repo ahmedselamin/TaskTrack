679aefbbc0faed5f434e6e9de82853da2d081b2c
eddyiiz <eddyxsayf@gmail.com>
2024-12-01 13:52:21 +0200

    remove

--- TaskTrack.Shared/UpdateTodoDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TaskTrack.Shared
-{
-    public class UpdateTodoDTO
-    {
-        public string Title { get; set; } = string.Empty;
-        public bool Status { get; set; } = false;
-        public DateTime Timestamp { get; set; } = DateTime.Now;
-    }
-}
