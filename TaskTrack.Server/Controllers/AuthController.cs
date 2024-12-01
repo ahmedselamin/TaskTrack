a26fee57ccbcd79aa55029eb6731e5f4b514f2b6
eddyiiz <eddyxsayf@gmail.com>
2024-12-01 12:02:24 +0200

    add register

---
+++ TaskTrack.Server/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskTrack.Server.Services.AuthService;
+
+namespace TaskTrack.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("Register")]
+        public async Task<ActionResult> Register([FromBody] UserRegisterDTO request)
+        {
+            var response = await _authService
+                .Register(
+                    new User
+                    {
+                        Username = request.Username,
+                    },
+                    request.Password
+                );
+
+            return response.Success ? Ok(response) : BadRequest(response.Message);
+        }
+    }
+}
