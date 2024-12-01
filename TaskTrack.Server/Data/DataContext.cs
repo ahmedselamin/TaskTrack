d8aa130fef82dafc9b68aabd72c1f9906ef63c11
eddyiiz <eddyxsayf@gmail.com>
2024-12-01 12:12:23 +0200

    add todos table

--- TaskTrack.Server/Data/DataContext.cs
+++ TaskTrack.Server/Data/DataContext.cs
@@ -3,5 +3,6 @@
     public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
     {
         public required DbSet<User> Users { get; set; }
+        public required DbSet<Todo> Todos { get; set; }
     }
 }
