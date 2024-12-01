edfd1332f0ce5fa1c876aa9f55aad38da793c90d
eddyiiz <eddyxsayf@gmail.com>
2024-12-01 11:16:42 +0200

    add model

---
+++ TaskTrack.Shared/ServiceResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskTrack.Shared
+{
+    public class ServiceResponse<T>
+    {
+        public T? Data { get; set; }
+        public string Message { get; set; } = "Successfull";
+        public bool Success { get; set; } = true;
+    }
+}
