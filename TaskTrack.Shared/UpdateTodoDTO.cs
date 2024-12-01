9f832800f18caf5799d73203935d5ecc19b4b1eb
eddyiiz <eddyxsayf@gmail.com>
2024-12-01 13:58:30 +0200

    add dto

---
+++ TaskTrack.Shared/UpdateTodoDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskTrack.Shared
+{
+    public class UpdateTodoDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public bool Status { get; set; } = false;
+    }
+}
+
