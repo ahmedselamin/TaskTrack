0b49a830ebb0d5f25acb5be3012aa333dad945c8
eddyiiz <eddyxsayf@gmail.com>
2024-12-01 12:10:21 +0200

    add model

---
+++ TaskTrack.Shared/Todo.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskTrack.Shared
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public bool status { get; set; } = false;
+        public DateTime Timestamp { get; set; } = DateTime.Now;
+    }
+}
