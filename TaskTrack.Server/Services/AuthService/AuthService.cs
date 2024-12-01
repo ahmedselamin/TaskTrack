aec0acd81729c307ec641387905a78d0e710de94
eddyiiz <eddyxsayf@gmail.com>
2024-12-01 11:46:32 +0200

    add register

--- TaskTrack.Server/Services/AuthService/AuthService.cs
+++ TaskTrack.Server/Services/AuthService/AuthService.cs
@@ -1,6 +1,113 @@
-﻿namespace TaskTrack.Server.Services.AuthService
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+
+namespace TaskTrack.Server.Services.AuthService
 {
     public class AuthService : IAuthService
     {
+        private readonly DataContext _context;
+        private readonly IConfiguration _config;
+
+        public AuthService(DataContext context, IConfiguration config)
+        {
+            _context = context;
+            _config = config;
+        }
+        public async Task<ServiceResponse<int>> Register(User user, string password)
+        {
+            var response = new ServiceResponse<int>();
+
+            try
+            {
+                if (await UserExists(user.Username))
+                {
+                    response.Success = false;
+                    response.Message = "Username unavailable";
+
+                    return response;
+                }
+
+                CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+                user.PasswordHash = passwordHash;
+                user.PasswordSalt = passwordSalt;
+
+                await _context.Users.AddAsync(user);
+                await _context.SaveChangesAsync();
+
+                response.Data = user.Id;
+                response.Message = "Welcome to TaskTrack!";
+
+                return response;
+            }
+            catch (Exception ex)
+            {
+                response.Success = false;
+                response.Message = ex.Message;
+
+                return response;
+            }
+        }
+
+        public Task<ServiceResponse<string>> Register(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+        public async Task<bool> UserExists(string username)
+        {
+            if (await _context.Users.AnyAsync(user => user.Username.ToLower()
+                     .Equals(username.ToLower())))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void CreatePasswordHash(string passowrd, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(passowrd));
+            }
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+                var computeHash =
+                    hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+
+                return computeHash.SequenceEqual(passwordHash);
+            }
+        }
+
+        private string CreateToken(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8
+                    .GetBytes(_config.GetSection("AppSettings:Token").Value));
+
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken
+                (
+                  claims: claims,
+                  expires: DateTime.Now.AddDays(1),
+                  signingCredentials: creds
+                );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        }
     }
 }
