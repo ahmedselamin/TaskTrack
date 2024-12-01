651cb61b8c77c3b932fe6b9b852b5b96931c956d
eddyiiz <eddyxsayf@gmail.com>
2024-12-01 11:52:45 +0200

    debug

--- TaskTrack.Server/Services/AuthService/IAuthService.cs
+++ TaskTrack.Server/Services/AuthService/IAuthService.cs
@@ -3,6 +3,6 @@
     public interface IAuthService
     {
         Task<ServiceResponse<int>> Register(User user, string password);
-        Task<ServiceResponse<string>> Register(string username, string password);
+        Task<ServiceResponse<string>> Login(string username, string password);
     }
 }
