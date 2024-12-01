7f73f9f16f30297077f10a1bcc51912f18f8042a
eddyiiz <eddyxsayf@gmail.com>
2024-12-01 12:17:28 +0200

    add relationship between user and todo

--- TaskTrack.Server/Data/DataContext.cs
+++ TaskTrack.Server/Data/DataContext.cs
@@ -4,5 +4,13 @@
     {
         public required DbSet<User> Users { get; set; }
         public required DbSet<Todo> Todos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Todos)
+                .WithOne()
+                .HasForeignKey(t => t.UserId);
+        }
     }
 }
--- TaskTrack.Shared/Todo.cs
+++ TaskTrack.Shared/Todo.cs
@@ -3,6 +3,7 @@
     public class Todo
     {
         public int Id { get; set; }
+        public int UserId { get; set; }
         public string Title { get; set; } = string.Empty;
         public bool status { get; set; } = false;
         public DateTime Timestamp { get; set; } = DateTime.Now;
