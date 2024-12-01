478d971117a9c5e547b560a0716f5784b82e80ff
eddyiiz <eddyxsayf@gmail.com>
2024-12-01 13:46:15 +0200

    debug

--- TaskTrack.Server/Services/TodoService/ITodoService.cs
+++ TaskTrack.Server/Services/TodoService/ITodoService.cs
@@ -4,8 +4,8 @@
     {
         Task<ServiceResponse<List<Todo>>> FetchTodos(int userId);
         Task<ServiceResponse<Todo>> FetchTodo(int userId, int todoId);
-        Task<ServiceResponse<Todo>> CreateTodo(int userId, Todo todo);
-        Task<ServiceResponse<Todo>> UpdateTodo(int userId, Todo updatedTodo);
+        Task<ServiceResponse<Todo>> CreateTodo(int userId, CreateTodoDTO newTodo);
+        Task<ServiceResponse<Todo>> UpdateTodo(int userId, UpdateTodoDTO updatedTodo);
         Task<ServiceResponse<bool>> DeleteTodo(int userId, int todoId);
     }
 }
